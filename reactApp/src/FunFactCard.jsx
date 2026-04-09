3b7f88f8a6fbe44bde2e7768e5b11d2f8fa9dbb3
konradkelly <konradky@gmail.com>
2026-04-09 10:56:18 -0700

    Initial commit

---
+++ reactApp/src/FunFactCard.jsx
@@ -0,0 +1,16 @@
+function FunFactCard () {
+    return (
+        <>
+    <div>
+      <h2>Fun Fact</h2>
+      <p>This is a fun fact.</p>
+    </div>
+    <div>
+      <h2>Fun Fact</h2>
+      <p>I like to eat Zongzi (Chinese Sticky Rice in Bamboo Leaves).</p>
+    </div>
+    </>    
+  );
+}
+
+export default FunFactCard;
