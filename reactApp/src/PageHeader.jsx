23ce5743171dee94249dea396725a6e8a30818de
konradkelly <konradky@gmail.com>
2026-04-09 18:01:27 -0700

    Add styled PageHeader component with names and divider

---
+++ reactApp/src/PageHeader.jsx
@@ -0,0 +1,11 @@
+function PageHeader() {
+  return (
+    <header className="page-header">
+      <h1>Fun Facts</h1>
+      <div className="header-divider"></div>
+      <p className="header-names">Konrad Kelly &amp; Sam Kosal</p>
+    </header>
+  );
+}
+
+export default PageHeader;
